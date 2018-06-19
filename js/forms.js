320815b5e1035c975a65778531af1de9c566754a
johelo <nriqlops@gmail.com>
2018-06-18 22:36:32 -0300

    Validação por tipo de campo

--- js/forms.js
+++ js/forms.js
@@ -2,6 +2,7 @@ $(document).ready(function()
 {
     $("span").hide();
 
+    // Campo obrigatório
     $("input[id*='_req_']").blur(function(){
         if($(this).val()==""){
             $(this).next().show();
@@ -12,8 +13,32 @@ $(document).ready(function()
         $(this).next().hide();
     });
 
+    // Campo combo
+    $("input[id*='_cbo_']").change(function(){
+        if($(this).val()==""){
+            $(this).next().show();
+        }
+    });
+
+    $("input[id*='_cbo_']").focus(function(){
+        $(this).next().hide();
+    });
+
+    // Campo email
+    $("input[id*='_mail_']").blur(function(){
+        var oEmailMask = new RegExp("^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$")
+        if(oEmailMask.test($(this).val()) == false){
+            $(this).next().show();
+        }
+    });
+
+    $("input[id*='_mail_']").focus(function(){
+        $(this).next().hide();
+    });
+    
+    // Campo data
     $("input[id*='_dat_']").blur(function(){
-        var oDateMask = new RegExp("^((((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))|(((0[1-9]|[12]\d|3[01])(0[13578]|1[02])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|[12]\d|30)(0[13456789]|1[012])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|1\d|2[0-8])02((1[6-9]|[2-9]\d)?\d{2}))|(2902((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00))))$")
+        var oDateMask = new RegExp(/^((((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))|(((0[1-9]|[12]\d|3[01])(0[13578]|1[02])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|[12]\d|30)(0[13456789]|1[012])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|1\d|2[0-8])02((1[6-9]|[2-9]\d)?\d{2}))|(2902((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00))))$/)
         if(oDateMask.test($(this).val()) == false){
             $(this).next().show();
         }
@@ -23,9 +48,24 @@ $(document).ready(function()
         $(this).next().hide();
     });
 
+    //Campo hora
+    $("input[id*='_hor_']").blur(function(){
+        if($(this).val != ""){
+            var oNumericMask = new RegExp(/^[0-2][0-3]:[0-5][0-9]$/)
+            if(oNumericMask.test($(this).val()) == false){
+                $(this).next().show();
+            }
+        }
+    });
+
+    $("input[id*='_hor_']").focus(function(){
+        $(this).next().hide();
+    });
+    
+    //Campo numérico
     $("input[id*='_num_']").blur(function(){
         if($(this).val != ""){
-            var oNumericMask = new RegExp("^[0-9]$")
+            var oNumericMask = new RegExp(/^[0-9]$/)
             if(oNumericMask.test($(this).val()) == false){
                 $(this).next().show();
             }
@@ -35,66 +75,34 @@ $(document).ready(function()
     $("input[id*='_num_']").focus(function(){
         $(this).next().hide();
     });
-/*
-    jQuery(function($) {
-        $(document).on('keypress', 'input.somente-numero', function(e) {
-            var key = (window.event)?event.keyCode:e.which;
-            if((key > 47 && key < 58)) {
-                return true;
-            } 
-            else {
-               return (key == 8 || key == 0)?true:false;
-            }
-        });
-    });
-    */
-    // Data
-    /*
-    $("#inputData").blur(function(){
-        if($(this).val() == ''){
-            $(this).addClass("fielderror");
-            $("#missingData").show();
-        } 
-    });
-    $("#inputData").focus(function(){
-        $("#inputData").addClass("");
-        $("span").hide();
-    });
 
-    // Hora
-    $("#inputTipoatividade").blur(function(){
-        if($(this).val() == ''){
-            $(this).addClass("fielderror");
-            $("#missingAtividade").show();
-        } 
-    });
-    $("#inputTipoatividade").focus(function(){
-        $("#inputTipoatividade").addClass("");
-        $("span").hide();
-    });
-
-    // Tipo de atividade
-    $("#inputHora").blur(function(){
-        if($(this).val() == ''){
-            $(this).addClass("fielderror");
-            $("#missingHora").show();
-        } 
-    });
-    $("#inputHora").focus(function(){
-        $("#inputHora").addClass("");
-        $("span").hide();
-    });
-
-    // Calorias
-    $("#inputCalorias").blur(function(){
-        if($(this).val() == '' || $(this).val()<1){
-            $(this).addClass("fielderror");
-            $("#missingCalorias").show();
-        } 
-    });
-    $("#inputCalorias").focus(function(){
-        $("#inputCalorias").addClass("");
-        $("span").hide();
+    //Campo decimal
+/*    $("input[id*='_dec_']").blur(function(){
+        checkregexfield(/^[0-2],[0-9][0-9]$/)
     });
 */
+    //Campo decimal
+    $("input[id*='_dec_']").blur(function(){
+        if($(this).val != ""){
+            var oNumericMask = new RegExp(/^[0-2],[0-9][0-9]$/)
+            if(oNumericMask.test($(this).val()) == false){
+                $(this).next().show();
+            }
+        }
+    });
+
+    $("input[id*='_dec_']").focus(function(){
+        $(this).next().hide();
+    });
+   
+    function checkregexfield(expr) {
+        field = $(this).val;
+        if(field != ""){
+            var oFieldMask = new RegExp(expr)
+            if(oFieldMask.test(field) == false){
+                $(this).next().show();
+            }
+        }
+    }    
+    
 });
