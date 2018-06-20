cc719632d771eacd8b5fd6d10c69f3cc1ab364e3
johelo <nriqlops@gmail.com>
2018-06-19 23:05:02 -0300

    Função javascript de validação unificada

--- js/forms.js
+++ js/forms.js
@@ -4,105 +4,52 @@ $(document).ready(function()
 
     // Campo obrigatório
     $("input[id*='_req_']").blur(function(){
-        if($(this).val()==""){
+        if($(this).val().length == 0){
             $(this).next().show();
         }
-    });
-
-    $("input[id*='_req_']").focus(function(){
-        $(this).next().hide();
-    });
-
-    // Campo combo
-    $("input[id*='_cbo_']").change(function(){
-        if($(this).val()==""){
-            $(this).next().show();
-        }
-    });
-
-    $("input[id*='_cbo_']").focus(function(){
-        $(this).next().hide();
     });
 
     // Campo email
     $("input[id*='_mail_']").blur(function(){
-        var oEmailMask = new RegExp("^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$")
-        if(oEmailMask.test($(this).val()) == false){
-            $(this).next().show();
-        }
+        checkregexfield($(this).attr("id"), /^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$/)
     });
 
-    $("input[id*='_mail_']").focus(function(){
-        $(this).next().hide();
-    });
-    
     // Campo data
     $("input[id*='_dat_']").blur(function(){
-        var oDateMask = new RegExp(/^((((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))|(((0[1-9]|[12]\d|3[01])(0[13578]|1[02])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|[12]\d|30)(0[13456789]|1[012])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|1\d|2[0-8])02((1[6-9]|[2-9]\d)?\d{2}))|(2902((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00))))$/)
-        if(oDateMask.test($(this).val()) == false){
-            $(this).next().show();
-        }
-    });
-
-    $("input[id*='_dat_']").focus(function(){
-        $(this).next().hide();
+        checkregexfield($(this).attr("id"), /^((((0?[1-9]|[12]\d|3[01])[\.\-\/](0?[13578]|1[02])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|[12]\d|30)[\.\-\/](0?[13456789]|1[012])[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|((0?[1-9]|1\d|2[0-8])[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?\d{2}))|(29[\.\-\/]0?2[\.\-\/]((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))|(((0[1-9]|[12]\d|3[01])(0[13578]|1[02])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|[12]\d|30)(0[13456789]|1[012])((1[6-9]|[2-9]\d)?\d{2}))|((0[1-9]|1\d|2[0-8])02((1[6-9]|[2-9]\d)?\d{2}))|(2902((1[6-9]|[2-9]\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00))))$/)
     });
 
     //Campo hora
     $("input[id*='_hor_']").blur(function(){
-        if($(this).val != ""){
-            var oNumericMask = new RegExp(/^[0-2][0-3]:[0-5][0-9]$/)
-            if(oNumericMask.test($(this).val()) == false){
-                $(this).next().show();
-            }
-        }
+        checkregexfield($(this).attr("id"), /^[0-2][0-3]:[0-5][0-9]$/)
     });
 
-    $("input[id*='_hor_']").focus(function(){
-        $(this).next().hide();
-    });
-    
     //Campo numérico
     $("input[id*='_num_']").blur(function(){
-        if($(this).val != ""){
-            var oNumericMask = new RegExp(/^[0-9]$/)
-            if(oNumericMask.test($(this).val()) == false){
-                $(this).next().show();
-            }
-        }
+        checkregexfield($(this).attr("id"), /^[0-9]$/)
     });
 
-    $("input[id*='_num_']").focus(function(){
-        $(this).next().hide();
-    });
-
-    //Campo decimal
-/*    $("input[id*='_dec_']").blur(function(){
-        checkregexfield(/^[0-2],[0-9][0-9]$/)
-    });
-*/
     //Campo decimal
     $("input[id*='_dec_']").blur(function(){
-        if($(this).val != ""){
-            var oNumericMask = new RegExp(/^[0-2],[0-9][0-9]$/)
-            if(oNumericMask.test($(this).val()) == false){
-                $(this).next().show();
-            }
-        }
+        checkregexfield($(this).attr("id"), /^[0-2],[0-9][0-9]$/)
     });
 
-    $("input[id*='_dec_']").focus(function(){
+    //Limpa mensagem
+    $("input[id^='input_']").focus(function(){
         $(this).next().hide();
     });
    
-    function checkregexfield(expr) {
-        field = $(this).val;
-        if(field != ""){
-            var oFieldMask = new RegExp(expr)
-            if(oFieldMask.test(field) == false){
-                $(this).next().show();
+    //Valida conteúdo
+    function checkregexfield(fieldid, regexpr) {
+        fieldvalue = $("#"+fieldid).val().length();
+        if(fieldvalue > 0){
+            var oFieldMask = new RegExp(regexpr)
+            if(oFieldMask.test(fieldvalue) == false){
+                $("#"+fieldid).next().show();
+                return 0;
             }
         }
+        return 1;
     }    
     
 });
--- js/login.js
+++ js/login.js
@@ -2,23 +2,26 @@ $(document).ready(function()
 {
     $("span").hide();
 
-    $("#inputEmail").blur(function(){
-        if ($(this).val().length == 0){
-            $(this).addClass("fielderror");
-            $("#missingEmail").show();
-        } 
-        else{
-            var oEmailMask = new RegExp("^([0-9a-zA-Z]+([_.-]?[0-9a-zA-Z]+)*@[0-9a-zA-Z]+[0-9,a-z,A-Z,.,-]*(.){1}[a-zA-Z]{2,4})+$")
-            if(oEmailMask.test($(this).val()) == false){
-                $(this).addClass("fielderror");
-                $("#invalidEmail").show();
-            }
-        }
-    });
-    
     $("#inputEmail").focus(function(){
-        $("#inputEmail").addClass("");
         $("span").hide();
     });
 
+    function validadelogin(){
+        if ($("#inputEmail").val().length == 0){
+            $("#inputEmail").next().show();
+            $("#inputEmail").focus();
+            return 0;
+        }
+
+        if ($("#inputPassword").val().length == 0){
+            $("#inputPassword").next().show();
+            $("#inputPassword").focus();
+            return 0;
+        }
+
+        submit();
+    }
+
 });
+
+//      <button class="btn btn-lg btn-primary btn-block" id="buttonEnter" type="submit">Entrar</button>
